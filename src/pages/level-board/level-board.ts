b69bb326fc26b784f45e398d7f42ad3ba71a5ec8
Heumsinat <heumsinatgic25@gmail.com>
2018-09-07 14:58:49 +0700

    update on level board

--- src/pages/level-board/level-board.ts
+++ src/pages/level-board/level-board.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component,  ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Content } from 'ionic-angular';
+import {DatabaseProvider} from "../../providers/database/database";
 
 /**
  * Generated class for the LevelBoardPage page.
@@ -14,17 +15,70 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'level-board.html',
 })
 export class LevelBoardPage {
+   @ViewChild('content') content:any;
 
-  levels: any = [1, 2, 3];
-  index: number = this.index + 8;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  levels: any = [
+    {'bg' : 'rgba(0, 128, 0, 0.966)'},
+    {'bg' : 'skyblue'},
+    {'bg' : 'blue'},
+    {'bg' : 'lightseagreen'},
+    {'bg' : 'lightgreen'},
+    {'bg' : 'lightcyan'},
+    {'bg' : 'blue'},
+    {'bg' : 'rgb(21, 248, 21)'},
+  ];
+  position: any = [
+    {'left' : '0px'},
+    {'left' : '230px'},
+    {'left' : '110px'},
+    {'left' : '10px'},
+    {'left' : '130px'},
+    {'left' : '230px'},
+    {'left' : '5px'},
+    {'left' : '150px'},
+  ]
+  // index: number = this.index + 8;
+ 
+   sections: any =[];
+   
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public db: DatabaseProvider) {
     // this.index = this.index + 1;
-    console.log('==========> index: ',this.index);
+    // console.log('==========> index: ',this.index);
+    this.getSections();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LevelBoardPage');
   }
+  ionViewDidEnter(){
+    this.content.scrollToBottom(0);
+  }
+  
+  getSections(){
+    this.db.executeSQL(`SELECT * FROM sections`)
+      .then(res => {
+        this.sections = [];
+        let bgIndex = 0;
+        let postionIndex =0;
+        for (var i = res.rows.length-1; i>=0; i--){
+          this.sections.push({
+            id: res.rows.item(i).id,
+            bg: this.levels[bgIndex].bg,
+            left: this.position[postionIndex].left,
+            
+
+          });
+          bgIndex++;
+          postionIndex++;
+          if(bgIndex == 8)
+            bgIndex = 0;
+          if(postionIndex == 8)
+          postionIndex = 0  
+        }
+      }).catch(e => console.log((e)))
+}
 
 }
